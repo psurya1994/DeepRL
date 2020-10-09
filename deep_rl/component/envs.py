7963f6c2a1294d42d948dd2208a43c4c7ba46f41
Surya Penmetsa <p.surya1994@gmail.com>
2020-10-09 08:00:24 -0400

    More code on representation learning

--- deep_rl/component/envs.py
+++ deep_rl/component/envs.py
@@ -20,6 +20,7 @@ from .fourrooms_dynamic import *
 from .lineworld import LineWorld
 from .memory_lineworld import *
 from gym_minigrid.wrappers import *
+from .gridworld_pixel import *
 
 from ..utils import *
 
@@ -90,6 +91,11 @@ def make_env(env_id, seed, rank, episode_life=True):
 
             elif(env_id == "MemoryLineWorld"):
                 env = MemoryLineWorld()
+                
+            elif(env_id == "PixelGridWorld"):
+                env = PixelGridWorld()
+#                 env = TransposeImage(env)
+                env=TransposeImageMini(env)
 
 
             # For everything else
@@ -122,6 +128,38 @@ def make_env(env_id, seed, rank, episode_life=True):
 
     return _thunk
 
+class RGBImgObsWrapper(gym.core.ObservationWrapper):
+    """
+    Wrapper to use fully observable RGB image as the only observation output,
+    no language/mission. This can be used to have the agent to solve the
+    gridworld in pixel space.
+    """
+
+    def __init__(self, env, tile_size=8):
+        super().__init__(env)
+
+        self.tile_size = tile_size
+
+        self.observation_space.spaces['image'] = spaces.Box(
+            low=0,
+            high=255,
+            shape=(84, 84, 3),
+            dtype='uint8'
+        )
+
+    def observation(self, obs):
+        env = self.unwrapped
+
+        rgb_img = env.render(
+            mode='rgb_array',
+            highlight=False,
+            tile_size=self.tile_size
+        )
+
+        return {
+            'mission': obs['mission'],
+            'image': rgb_img
+        }
 
 class OriginalReturnWrapper(gym.Wrapper):
     def __init__(self, env):
